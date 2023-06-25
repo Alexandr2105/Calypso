4c1e961314d9e417e956f932a1ecaf8fcf9ef507
Alexandr2105 <5030553@gmail.com>
2023-06-25 13:13:44 +0300

    swagger doc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { config } from 'dotenv';
+config();
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'swagger-static'),
+      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
