0ae133e1a7961a539deec1c2e4a8014872cc414f
VladislavShults <shvs93@mail.ru>
2023-06-27 10:08:21 +0500

    fix4

--- src/common/bcript/bcript.service.ts
+++ src/common/bcript/bcript.service.ts
@@ -1,9 +1,10 @@
-import * as bcrypt from 'bcrypt';
+// import * as bcrypt from 'bcrypt';
 
 export class BcryptService {
   async generateHash(password: string) {
-    const saltRounds = 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-    return await bcrypt.hash(password, salt);
+    // const saltRounds = 10;
+    // const salt = await bcrypt.genSalt(saltRounds);
+    // return await bcrypt.hash(password, salt);
+    return 'hfhafwhfawff67afawglfajgfwa';
   }
 }
