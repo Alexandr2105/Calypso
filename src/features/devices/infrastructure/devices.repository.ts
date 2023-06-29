e653b8d331f17eae866e6fcd57e87693661a4cc0
Alexandr2105 <5030553@gmail.com>
2023-06-29 11:54:58 +0300

    fix and refactoring devices.repository.ts

--- src/features/devices/infrastructure/devices.repository.ts
+++ src/features/devices/infrastructure/devices.repository.ts
@@ -7,30 +7,24 @@ export class DevicesRepository {
   constructor(@Inject(PrismaService) private prisma: PrismaService) {}
 
   async getInfoAboutDeviceUser(userId: string, deviceId: string) {
-    const devicesInfo = await this.prisma.refreshTokenData.findFirst({
+    return this.prisma.refreshTokenData.findFirst({
       where: { userId: userId, deviceId: deviceId },
     });
-    console.log(devicesInfo);
-    return devicesInfo;
   }
 
-  async delDevice(deviceId: string): Promise<boolean> {
-    const a = await this.prisma.refreshTokenData.delete({
+  async delDevice(deviceId: string): Promise<void> {
+    await this.prisma.refreshTokenData.delete({
       where: { deviceId: deviceId },
     });
-    console.log(a);
-    return true;
   }
 
   async saveInfoRefreshToken(info: RefreshTokenDataEntity) {
-    const a = await this.prisma.refreshTokenData.create({ data: info });
-    console.log(a);
-    return a;
+    await this.prisma.refreshTokenData.create({ data: info });
   }
 
   async delOldRefreshTokenData(date: number) {
     await this.prisma.refreshTokenData.deleteMany({
-      where: { exp: { lt: date / 1000 } },
+      where: { exp: { lt: Math.floor(date / 1000) } },
     });
   }
 }
