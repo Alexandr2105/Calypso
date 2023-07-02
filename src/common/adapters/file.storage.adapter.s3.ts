5ff25f9bb9e8904089fbb674c670cb20a76624bc
Alexandr2105 <5030553@gmail.com>
2023-07-02 14:50:36 +0300

    fix file.storage.adapter.s3.ts

--- src/common/adapters/file.storage.adapter.s3.ts
+++ src/common/adapters/file.storage.adapter.s3.ts
@@ -23,51 +23,11 @@ export class FileStorageAdapterS3 {
       Body: buffer,
       ContentType: 'image/png',
     });
-
-    console.log(buffer);
     try {
       await this.s3Client.send(command);
-      console.log(command);
       return `${userId}/avatars/${userId}_avatar.png`;
     } catch (err) {
       console.error(err);
     }
   }
-
-  // async saveImageForPost(
-  //   userId: string,
-  //   fileName: string,
-  //   wallpaperBuffer: Buffer,
-  //   folderName: string,
-  //   blogId: string,
-  //   postId: string,
-  //   infoSize: string,
-  // ): Promise<ImageModelDocument> {
-  //   const command = new PutObjectCommand({
-  //     Bucket: 'my1bucket',
-  //     Key: `images/${folderName}/${postId}_post_${infoSize}.png`,
-  //     Body: wallpaperBuffer,
-  //     ContentType: 'image/png',
-  //   });
-  //   try {
-  //     await this.s3Client.send(command);
-  //     const newImage = new this.image();
-  //     newImage.id = +new Date() + '';
-  //     newImage.blogId = blogId;
-  //     newImage.key = `images/${folderName}/${postId}_post_${infoSize}.png`;
-  //     newImage.bucket = 'my1bucket';
-  //     newImage.postId = postId;
-  //     return newImage;
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // }
-
-  // async getImage(bucket: string, key: string) {
-  //   const command = new GetObjectCommand({
-  //     Bucket: bucket,
-  //     Key: key,
-  //   });
-  //   const a = await this.s3Client.send(command);
-  // }
 }
