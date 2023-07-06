e8d05c87dac0dd4d5e30911ccd49ece924a9b31d
Alexandr2105 <5030553@gmail.com>
2023-07-06 18:17:58 +0300

    fix length description

--- src/features/posts/dto/description.dto.ts
+++ src/features/posts/dto/description.dto.ts
@@ -3,6 +3,6 @@ import { Transform } from 'class-transformer';
 
 export class DescriptionDto {
   @Transform(({ value }) => String(value).trim())
-  @Length(0, 3)
+  @Length(0, 500)
   description: string;
 }
