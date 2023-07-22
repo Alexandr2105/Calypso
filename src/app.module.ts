9d2521565307d7b195dd8e4d64d2baa8fdd9c9f5
Alexandr2105 <5030553@gmail.com>
2023-07-22 12:48:22 +0300

    add rabbitmq 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -87,7 +87,7 @@ const Repositories = [
         transport: Transport.TCP,
         options: {
           port: 3001,
-          host: 'calipso-microservice-files.vercel.app',
+          // host: 'calipso-microservice-files.vercel.app',
         },
       },
     ]),
