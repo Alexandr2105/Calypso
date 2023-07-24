7b979ffd80081c3ec01187db8a0d9ffd3536d51c
Alexandr2105 <5030553@gmail.com>
2023-07-24 16:59:37 +0300

    del port and add host

--- src/app.module.ts
+++ src/app.module.ts
@@ -86,8 +86,8 @@ const Repositories = [
         name: 'FILES_SERVICE',
         transport: Transport.TCP,
         options: {
-          port: 3002,
-          // host: 'calipso-microservice-files.onrender.com',
+          // port: 3002,
+          host: 'calipso-microservice-files.onrender.com',
         },
       },
     ]),
