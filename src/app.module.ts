c0230f66cacf51fb004ac8c846acd04c7bb3ab4f
Alexandr2105 <5030553@gmail.com>
2023-07-27 14:38:05 +0300

    update host

--- src/app.module.ts
+++ src/app.module.ts
@@ -87,7 +87,7 @@ const Repositories = [
         transport: Transport.TCP,
         options: {
           port: 3001,
-          // host: 'calypso-microservice-files.fly.dev',
+          host: '54.220.192.176',
           // host: 'calypso-microservice-files.fly.dev',
         },
       },
