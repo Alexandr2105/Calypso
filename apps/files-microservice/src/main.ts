6cd4ea1b0b57118a1281d10fef46b38483e1fd54
Alexandr2105 <5030553@gmail.com>
2023-07-31 19:32:39 +0300

    fix vercel.json

--- apps/files-microservice/src/main.ts
+++ apps/files-microservice/src/main.ts
@@ -1,29 +1,34 @@
 import { NestFactory } from '@nestjs/core';
-import { FilesMicroserviceModule } from './files-microservice.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { FilesMicroserviceModule } from './files-microservice.module';
 
 export async function bootstrap() {
-  const rawApp = await NestFactory.create(FilesMicroserviceModule);
-  await rawApp.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [
-        'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
-      ],
-      queue: 'FILES_SERVICE_RMQ',
-      queueOptions: {
-        durable: false,
+  const microserviceRMQ =
+    await NestFactory.createMicroservice<MicroserviceOptions>(
+      FilesMicroserviceModule,
+      {
+        transport: Transport.RMQ,
+        options: {
+          urls: [
+            'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
+          ],
+          queue: 'FILES_SERVICE_RMQ',
+          queueOptions: {
+            durable: false,
+          },
+        },
       },
-    },
-  });
-  // await microserviceRMQ.listen();
+    );
+  await microserviceRMQ.listen();
 
-  // await rawApp.connectMicroservice<MicroserviceOptions>({
-  //   transport: Transport.TCP,
-  //   options: { port: 3001 },
-  // });
-  // await microserviceTCP.listen();
-  await rawApp.startAllMicroservices();
-  await rawApp.listen(3002);
+  const microserviceTCP =
+    await NestFactory.createMicroservice<MicroserviceOptions>(
+      FilesMicroserviceModule,
+      {
+        transport: Transport.TCP,
+        options: { port: 3001 },
+      },
+    );
+  await microserviceTCP.listen();
 }
 bootstrap();
