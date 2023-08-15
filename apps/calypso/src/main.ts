e1cca1afdcaa7cb74d736abf2909aee08b1fe230
Alexandr2105 <5030553@gmail.com>
2023-08-15 16:38:54 +0300

    full app

--- apps/calypso/src/main.ts
+++ apps/calypso/src/main.ts
@@ -41,7 +41,7 @@ export async function bootstrap() {
 
   const rawApp = await NestFactory.create(AppModule);
   const app = createApp(rawApp);
-  // app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1');
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalFilters(new HttpExceptionFilter());
 
