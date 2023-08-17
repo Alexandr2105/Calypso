a0a654992a218cd330d3c6c5ab88f9103935659a
Alexandr2105 <5030553@gmail.com>
2023-08-17 10:42:03 +0300

    test 5 vercel monorepo files service

--- apps/files-microservice/src/main.ts
+++ apps/files-microservice/src/main.ts
@@ -4,51 +4,31 @@ import { FilesMicroserviceModule } from './files-microservice.module';
 import { settings } from './settings';
 
 export async function bootstrap() {
-  const app = await NestFactory.create(FilesMicroserviceModule);
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: { port: 3001 },
-  });
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [settings.RABBIT_MQ],
-      queue: 'FILES_SERVICE_RMQ',
-      queueOptions: {
-        durable: false,
+  const microserviceRMQ =
+    await NestFactory.createMicroservice<MicroserviceOptions>(
+      FilesMicroserviceModule,
+      {
+        transport: Transport.RMQ,
+        options: {
+          urls: [settings.RABBIT_MQ],
+          queue: 'FILES_SERVICE_RMQ',
+          queueOptions: {
+            durable: false,
+          },
+        },
       },
-    },
-  });
+    );
+  await microserviceRMQ.listen();
 
-  await app.startAllMicroservices();
-  await app.listen(3002);
-  // const microserviceRMQ =
-  //   await NestFactory.createMicroservice<MicroserviceOptions>(
-  //     FilesMicroserviceModule,
-  //     {
-  //       transport: Transport.RMQ,
-  //       options: {
-  //         urls: [settings.RABBIT_MQ],
-  //         queue: 'FILES_SERVICE_RMQ',
-  //         queueOptions: {
-  //           durable: false,
-  //         },
-  //       },
-  //     },
-  //   );
-  // await microserviceRMQ.listen();
-  //
-  // const microserviceTCP =
-  //   await NestFactory.createMicroservice<MicroserviceOptions>(
-  //     FilesMicroserviceModule,
-  //     {
-  //       transport: Transport.TCP,
-  //       options: { port: 3001 },
-  //     },
-  //   );
-  // await microserviceTCP.listen();
+  const microserviceTCP =
+    await NestFactory.createMicroservice<MicroserviceOptions>(
+      FilesMicroserviceModule,
+      {
+        transport: Transport.TCP,
+        options: { port: 3001 },
+      },
+    );
+  await microserviceTCP.listen();
 
   console.log('Microservices are starting 3002');
 }
