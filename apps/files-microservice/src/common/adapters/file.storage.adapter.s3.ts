525c4a0be377b94fd31f04d3a95bf1d19328e4fe
Alexandr2105 <5030553@gmail.com>
2023-08-20 13:30:27 +0300

    add console.log in upload.avatar.use.case.ts

--- apps/files-microservice/src/common/adapters/file.storage.adapter.s3.ts
+++ apps/files-microservice/src/common/adapters/file.storage.adapter.s3.ts
@@ -42,7 +42,7 @@ export class FileStorageAdapterS3 {
       console.log(settings.BASE_URL_AWS);
       console.log(settings.SECRET_ACCESS_KEY);
       console.log(settings.BASE_URL_AWS);
-      console.log(this.s3Client);
+      console.log(this.s3Client.config.region);
       await this.s3Client.send(command);
       console.log({
         id: randomUUID(),
