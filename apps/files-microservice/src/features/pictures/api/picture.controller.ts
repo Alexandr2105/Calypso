fe289c847b861cdb359bc3ab99c894c558dd92e5
Alexandr2105 <5030553@gmail.com>
2023-08-20 12:12:56 +0300

    test picture.controller.ts

--- apps/files-microservice/src/features/pictures/api/picture.controller.ts
+++ apps/files-microservice/src/features/pictures/api/picture.controller.ts
@@ -16,7 +16,7 @@ export class PictureController {
 
   @MessagePattern({ cmd: 'saveAvatar' })
   async saveAvatars(data: AvatarsDto): Promise<string> {
-    console.log('Пришел');
+    console.log(data.userId, data.avatar);
     return this.commandBus.execute(
       new UploadAvatarCommand(data.userId, data.avatar),
     );
