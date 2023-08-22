9c1c7d6f5f57027d19e0f1ec6dcbb040cc435d65
Alexandr2105 <5030553@gmail.com>
2023-08-22 19:46:20 +0300

    test 2 file.storage.adapter.s3.ts

--- apps/files-microservice/src/common/adapters/file.storage.adapter.s3.ts
+++ apps/files-microservice/src/common/adapters/file.storage.adapter.s3.ts
@@ -13,6 +13,10 @@ import { ApiConfigService } from '../helpers/api.config.service';
 export class FileStorageAdapterS3 {
   s3Client: S3Client;
   constructor(private apiConfigService: ApiConfigService) {
+    console.log(apiConfigService.bucketName);
+    console.log(apiConfigService.s3Region);
+    console.log(apiConfigService.secretAccessKey);
+    console.log(apiConfigService.accessKeyId);
     this.s3Client = new S3Client({
       region: apiConfigService.s3Region,
       endpoint: apiConfigService.baseUrlAws,
